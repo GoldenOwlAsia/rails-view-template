24f7d5751b81f9170da18ff7beb08d0b80896cac
jacky <jacky.le.goldenowl@gmail.com>
2025-03-06 16:07:33 +0700

    chore: improve datetime helper

--- app/helpers/date_time_helper.rb
+++ app/helpers/date_time_helper.rb
@@ -1,45 +1,45 @@
 module DateTimeHelper
-  def display_date(datetime = Date.current, format = nil)
-    strtime = format_date[format] || '%d/%m/%Y'
-    datetime.strftime(strtime)
+  def display_date(datetime = Date.current, format = :dmy_slash)
+    format_datetime_string(datetime, format, :date)
   end
 
-  def display_time(datetime = DateTime.current, format = nil)
-    strtime = format_time[format] || '%H:%M'
-    datetime.strftime(strtime)
+  def display_time(datetime = Time.zone.now, format = :hm24)
+    format_datetime_string(datetime, format, :time)
   end
 
-  def display_datetime(datetime = DateTime.current, format = nil)
-    strtime = format_datetime[format] || '%d/%m/%Y %H:%M'
-    datetime.strftime(strtime)
+  def display_datetime(datetime = Time.zone.now, format = :dmy_hm24)
+    format_datetime_string(datetime, format, :datetime)
   end
 
   private
 
-  def format_date
-    {
-      dmy_dashed: '%Y-%m-%d',
-      dmy_slash: '%d/%m/%Y',
-      dBy: '%d %B %Y',
-      dby: '%d %b %Y'
-    }
+  def format_datetime_string(datetime, format, type)
+    return '' if datetime.nil?
+
+    format_string = datetime_formats[type][format] || datetime_formats[type].values.first
+    datetime.strftime(format_string)
   end
 
-  def format_time
+  def datetime_formats
     {
-      hm24: '%H:%M',
-      hm12: '%I:%M',
-      hms24: '%H:%M:%S',
-      hms12: '%I:%M:%S'
-    }
-  end
-
-  def format_datetime
-    {
-      dmy_hm24: '%d/%m/%Y %H:%M',
-      dmy_hm12: '%d/%m/%Y %I:%M',
-      dmy_hms24: '%d/%m/%Y %H:%M:%S',
-      dmy_hms12: '%d/%m/%Y %I:%M:%S'
+      date: {
+        dmy_dashed: '%Y-%m-%d',
+        dmy_slash: '%d/%m/%Y',
+        dBy: '%d %B %Y',
+        dby: '%d %b %Y'
+      },
+      time: {
+        hm24: '%H:%M',
+        hm12: '%I:%M %p',
+        hms24: '%H:%M:%S',
+        hms12: '%I:%M:%S %p'
+      },
+      datetime: {
+        dmy_hm24: '%d/%m/%Y %H:%M',
+        dmy_hm12: '%d/%m/%Y %I:%M %p',
+        dmy_hms24: '%d/%m/%Y %H:%M:%S',
+        dmy_hms12: '%d/%m/%Y %I:%M:%S %p'
+      }
     }
   end
 end
